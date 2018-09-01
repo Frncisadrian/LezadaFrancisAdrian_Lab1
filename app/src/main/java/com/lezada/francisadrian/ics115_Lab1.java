98fa94ccfaca7b026e2e6d056abf7912c98b22be
frncisadrian <2015081640@ust-ics.mygbiz.com>
2018-09-01 15:40:44 +0800

    Initial commit

---
+++ app/src/main/java/com/lezada/francisadrian/ics115_Lab1.java
@@ -0,0 +1,13 @@
+package com.lezada.francisadrian;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ics115_Lab1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ics115__lab1);
+    }
+}
